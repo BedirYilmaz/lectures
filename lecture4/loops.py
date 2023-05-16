4ecd0aabf0cdb66ef41fa31a79e4b8f0378adf99
BedirYilmaz <BedirYilmaz@github.com>
2023-05-16 20:31:08 +0200

    loops

---
+++ lecture4/loops.py
@@ -0,0 +1,21 @@
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+# print("all day work and no play makes billy a dully boy")
+
+# for _ in range(13):
+#     print("all day work and no play makes billy a dully boy")
+
+i = 0
+while i < 13:
+    print("all day work and no play makes billy a dully boy")
+    i += 1
