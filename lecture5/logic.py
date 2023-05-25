c9a0ddeeafa76b53cd762f2689094d49d1ff9bdf
BedirYilmaz <BedirYilmaz@github.com>
2023-05-25 09:04:52 +0200

    if statements and boolean operators

---
+++ lecture5/logic.py
@@ -0,0 +1,11 @@
+# guessing game
+
+inp = 6 * 2
+target = 12
+
+if inp > target:
+    print("too large!")
+if inp < target:
+    print("too small!")
+if inp == target:
+    print("EXACTLY!!! YOU WON!!")
